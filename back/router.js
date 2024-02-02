0d5f9bda717d9d174c885be1604608a2af5da787
Jennifer Comlanvi <jennifer@sopheos.com>
2024-02-02 13:26:30 +0100

    versionning api back

--- back/router.js
+++ back/router.js
@@ -1,17 +1,17 @@
-const Router = require('@koa/router');
-const auth = require('./middleware/auth.js');
+const Router = require("@koa/router");
+const auth = require("./middleware/auth.js");
 
 const router = new Router();
 
-router.get('/', (ctx, next) => {
-    ctx.body = 'Hello';
+router.get("/", (ctx, next) => {
+  ctx.body = "Hello";
 });
 
-router.post('/login', require('./controllers/login.js'));
-router.post('/signin', require('./controllers/signin.js'));
-router.post('/refresh', require('./controllers/refresh.js'));
-router.get('/test', require('./controllers/test.js'));
+router.post("/api/v1/login", require("./controllers/login.js"));
+router.post("/api/v1/signin", require("./controllers/signin.js"));
+router.post("/api/v1/refresh", require("./controllers/refresh.js"));
+router.get("/api/v1/test", require("./controllers/test.js"));
 // router.get('/test', auth, require('./controllers/test.js'));
-router.post('/teams', require('./controllers/teams/create.js'));
+router.post("/api/v1/teams", require("./controllers/teams/create.js"));
 
 module.exports = router;
