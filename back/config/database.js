8ae2756d727ef4f98c2f08fa8dc53a777e8a8f14
Jennifer Comlanvi <jennifer@sopheos.com>
2024-02-10 16:56:49 +0100

    optimisation docker dev

---
+++ back/config/database.js
@@ -0,0 +1,29 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: "web",
+    database: "time_manager",
+    host: "127.0.0.1",
+    port: 3306,
+    dialect: "mariadb",
+    migrationStorageTableName: "_migrations",
+  },
+  test: {
+    username: "root",
+    password: "web",
+    database: "time_manager",
+    host: "127.0.0.1",
+    port: 3307,
+    dialect: "mariadb",
+    migrationStorageTableName: "_migrations",
+  },
+  production: {
+    username: process.env.DB_USERNAME || undefined,
+    password: process.env.DB_PASSWORD || undefined,
+    database: process.env.DB_DBNAME || undefined,
+    host: process.env.DB_HOST || undefined,
+    port: process.env.DB_PORT || undefined,
+    dialect: "mariadb",
+    migrationStorageTableName: "_migrations",
+  },
+};
